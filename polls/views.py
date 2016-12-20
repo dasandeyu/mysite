bc9abb11c44a0037ab56626b473ff331d7555ea5
wb-lx194092 <wb-lx194092@alipay.com>
2016-12-20 18:41:03 +0800

    如何给项目添加url地址和view

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello world, You'er at the polls index.")
+
+
+def detail(request, question_id):
+    print "question_id =", question_id
+    return HttpResponse("You're looking at question %s" % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
