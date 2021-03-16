392e81e8ddb8b3c90302aa52253152aed63133b6
lanxiang_sh <lanxiang_sh@ushareit.com>
2021-03-16 21:25:47 +0800

    ”新增4个视图函数”

--- polls/urls.py
+++ polls/urls.py
@@ -14,5 +14,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/results/
+    path('<int:question_id>/result/', views.result, name='result'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
